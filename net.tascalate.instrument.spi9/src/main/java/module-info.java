178b4541412f212ba70c2d6a1c9584dad14dce9f
vsilaev <vsilaev@gmail.com>
2019-02-22 17:28:22 +0300

    Fix license; minor formatting

--- net.tascalate.instrument.spi9/src/main/java/module-info.java
+++ net.tascalate.instrument.spi9/src/main/java/module-info.java
@@ -30,26 +30,12 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
- *
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/
- *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
- */
-/**
  * @author vsilaev
  *
  */
 module net.tascalate.instrument.spi {
     // For Java Agent / Bytecode Weaving Tools authors
-	exports net.tascalate.instrument.spi;
-	// For users of bytecode weaving tools
-	exports net.tascalate.instrument.api;
+    exports net.tascalate.instrument.spi;
+    //For users of bytecode weaving tools
+    exports net.tascalate.instrument.api;
 } 
